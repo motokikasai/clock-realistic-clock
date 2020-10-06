ba9ab292515294720b16c880bf22545f553f8b4a
motokikasai <kmomoky@hotmail.com>
2020-10-07 00:32:11 +0200

    Make three arms/needles work, except for transition issue at 0deg

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ function setDate() {
 
   const seconds = now.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
+
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-  //   console.log(secondsDegree);
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);

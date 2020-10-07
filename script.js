f49e8b22d621d515ceaca78e3230f326c17ad140
motokikasai <kmomoky@hotmail.com>
2020-10-07 11:00:24 +0200

    Fix transition issue into 0

--- script.js
+++ script.js
@@ -7,16 +7,33 @@ function setDate() {
 
   const seconds = now.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
-
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  // Fix secondhand reverse transition between 59 & 0 sec
+  if (seconds === 0) {
+    secondHand.classList.add('hand-transition');
+  } else {
+    secondHand.classList.remove('hand-transition');
+  }
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  // Fix minhand reverse transition between 59 & 0 sec
+  if (mins === 0) {
+    minHand.classList.add('hand-transition');
+  } else {
+    minHand.classList.remove('hand-transition');
+  }
 
   const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  // Fix hourhand reverse transition between 23 & 0 sec
+  if (hour === 0) {
+    hourHand.classList.add('hand-transition');
+  } else {
+    hourHand.classList.remove('hand-transition');
+  }
 }
 
 setInterval(setDate, 1000);
